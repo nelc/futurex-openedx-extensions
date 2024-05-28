14fb93cfa96e5f24c8d3d9ac3a4f26e4c91e5b60
Shadi Naif <shadinaif@gmail.com>
2024-05-28 20:13:52 +0300

    fix: missing request in serializer context

--- futurex_openedx_extensions/__init__.py
+++ futurex_openedx_extensions/__init__.py
@@ -1,3 +1,3 @@
 """One-line description for README and other doc files."""
 
-__version__ = '0.3.2'
+__version__ = '0.3.5'
--- test_utils/edx_platform_mocks/fake_models/functions.py
+++ test_utils/edx_platform_mocks/fake_models/functions.py
@@ -3,17 +3,17 @@
 from django.contrib.auth import get_user_model
 
 
-def get_course_blocks_completion_summary(course_key, user):  # pylint: disable=unused-argument
+def get_course_blocks_completion_summary(course_key, user):  # pylint: disable=unused-argument, useless-return
     """get_course_blocks_completion_summary Mock"""
     if not isinstance(user, get_user_model()):
-        raise Exception(f'Expects a user object but got "{user}" of type "{type(user)}"')
+        raise TypeError(f'Expects a user object but got "{user}" of type "{type(user)}"')
     return None
 
 
-def get_block_structure_manager(course_key):  # pylint: disable=unused-argument
+def get_block_structure_manager(course_key):
     """get_block_structure_manager Mock"""
     if not isinstance(course_key, str):
-        raise Exception(f'Expects a coruse_key as string but got "{course_key}" of type "{type(course_key)}"')
+        raise TypeError(f'Expects a coruse_key as string but got "{course_key}" of type "{type(course_key)}"')
 
     class Dummy:  # pylint: disable=too-few-public-methods
         """dummy class"""
@@ -27,5 +27,5 @@ def get_block_structure_manager(course_key):  # pylint: disable=unused-argument
 def get_certificates_for_user_by_course_keys(user, course_keys):  # pylint: disable=unused-argument
     """get_certificates_for_user_by_course_keys Mock"""
     if not isinstance(user, get_user_model()):
-        raise Exception(f'Expects a user object but got "{user}" of type "{type(user)}"')
+        raise TypeError(f'Expects a user object but got "{user}" of type "{type(user)}"')
     return {}
