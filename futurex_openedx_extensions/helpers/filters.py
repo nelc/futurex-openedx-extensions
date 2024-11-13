f37cdb3e81d5a1f63d0a853fa0af2be27014ab30
Tehreem Sadat <ts.tehreem@gmail.com>
2024-11-13 23:28:09 +1300

    feat: add search on notes and filename for csv tasks

--- futurex_openedx_extensions/helpers/filters.py
+++ futurex_openedx_extensions/helpers/filters.py
@@ -1,6 +1,10 @@
 """Filters helpers and classes for the API views."""
-from rest_framework.filters import OrderingFilter
+from rest_framework.filters import OrderingFilter, SearchFilter
 
 
 class DefaultOrderingFilter(OrderingFilter):
     ordering_param = 'sort'
+
+
+class DefaultSearchFilter(SearchFilter):
+    search_param = 'search_text'
