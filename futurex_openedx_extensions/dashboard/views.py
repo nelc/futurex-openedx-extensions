3592947a7a4b4b01edcd532f1a711b3619d6f2fa
shadinaif <shadinaif@gmail.com>
2025-06-14 09:45:25 +0300

    refactor: rename CourseAssetsManagementView
    rename it to TenantAssetsManagementView

--- futurex_openedx_extensions/dashboard/views.py
+++ futurex_openedx_extensions/dashboard/views.py
@@ -1642,10 +1642,10 @@ class FileUploadView(FXViewRoleInfoMixin, APIView):
         )
 
 
-@docs('CourseAssetsManagementView.create')
-@docs('CourseAssetsManagementView.list')
+@docs('TenantAssetsManagementView.create')
+@docs('TenantAssetsManagementView.list')
 @exclude_schema_for('retrieve', 'update', 'partial_update', 'destroy')
-class CourseAssetsManagementView(FXViewRoleInfoMixin, viewsets.ModelViewSet):  # pylint: disable=too-many-ancestors
+class TenantAssetsManagementView(FXViewRoleInfoMixin, viewsets.ModelViewSet):  # pylint: disable=too-many-ancestors
     """View to list and retrieve course assets."""
     authentication_classes = default_auth_classes
     permission_classes = [FXHasTenantAllCoursesAccess]
